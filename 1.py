66b895bc8304c94d014e8fede370c267444645b7
Karthik2511 <95971441+Karthik2511@users.noreply.github.com>
2024-12-05 23:29:58 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-#program to find largest number in a list
-a=[]
-n=int(input("Enter number of elements :"))
-for i in range (1,n+1):
- b=int(input("Enter element :"))
- a.append(b)
-print("largest element is ",max(a))
